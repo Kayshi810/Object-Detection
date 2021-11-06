fca57415175afda70b2aedf450576b24a072557d
Kayshi810 <78273109+Kayshi810@users.noreply.github.com>
2021-11-05 19:58:46 -0500

    Add files via upload

---
+++ couch.js
@@ -0,0 +1,34 @@
+img="";
+status="";
+function preload()
+{
+    img=loadImage('IMG-0364.jpg');
+}
+
+function setup()
+{
+    canvas=createCanvas(640, 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model is loaded");
+    status= true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error , results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        console.log(results);
+    }
+
+}
